f611b6905fc7464aac0e843f124ee5360845dd20
Hawdenn <magisk680@gmail.com>
2024-02-19 17:38:34 +0800

    Ganti Portofolio

---
+++ assets/js/custom.js
@@ -0,0 +1,88 @@
+
+$(document).ready(function() {
+    clockUpdate();
+    setInterval(clockUpdate, 1000);
+  })
+  
+  function clockUpdate() {
+    var date = new Date();
+    $('.digital-clock').css({'color': '#fff', 'text-shadow': '0 0 6px #ff0'});
+    function addZero(x) {
+      if (x < 10) {
+        return x = '0' + x;
+      } else {
+        return x;
+      }
+    }
+  
+    function twelveHour(x) {
+      if (x > 12) {
+        return x = x - 12;
+      } else if (x == 0) {
+        return x = 12;
+      } else {
+        return x;
+      }
+    }
+  
+    var h = addZero(twelveHour(date.getHours()));
+    var m = addZero(date.getMinutes());
+    var s = addZero(date.getSeconds());
+  
+    $('.digital-clock').text(h + ':' + m + ':' + s)
+  }
+  
+	var date = '03/12/2004';
+ 
+	if(date === ""){
+		alert("Please complete the required field!");
+    }else{
+		var today = new Date();
+		var birthday = new Date(date);
+		var year = 0;
+		if (today.getMonth() < birthday.getMonth()) {
+			year = 1;
+		} else if ((today.getMonth() == birthday.getMonth()) && today.getDate() < birthday.getDate()) {
+			year = 1;
+		}
+		var age = today.getFullYear() - birthday.getFullYear() - year;
+ 
+		if(age < 0){
+			age = 0;
+		}
+		document.getElementById('age').innerHTML = age;
+	}
+
+
+    /*!
+ * Dark Mode Switch v1.0.1 (https://github.com/coliff/dark-mode-switch)
+ * Copyright 2021 C.Oliff
+ * Licensed under MIT (https://github.com/coliff/dark-mode-switch/blob/main/LICENSE)
+ */
+var darkSwitch = document.getElementById("darkSwitch");
+window.addEventListener("load", (function () {
+    if (darkSwitch) {
+        initTheme();
+        darkSwitch.addEventListener("change", (function () {
+            resetTheme()
+        }))
+    }
+}));
+
+function initTheme() {
+    var darkThemeSelected = localStorage.getItem("darkSwitch") !== null && localStorage.getItem("darkSwitch") === "dark";
+    darkSwitch.checked = darkThemeSelected;
+    darkThemeSelected ? document.body.setAttribute("data-theme", "dark") : document.body.removeAttribute("data-theme")
+}
+
+function resetTheme() {
+    if (darkSwitch.checked) {
+        document.body.setAttribute("data-theme", "dark");
+        localStorage.setItem("darkSwitch", "dark")
+    } else {
+        
+        document.body.removeAttribute("data-theme");
+        localStorage.removeItem("darkSwitch")
+    }
+}
+
